d068f9858f27cdd4b0336609032958c8f5aff8b1
Alex Simonian <alex.simonian@gmail.com>
2017-05-11 21:24:03 -0500

    Add search method

--- Spider.java
+++ Spider.java
@@ -24,7 +24,21 @@ public class Spider {
         while(this.pagesVisited.size() < MAX_PAGES_TO_SEARCH) {
             String currentUrl;
             SpiderLeg leg = new SpiderLeg();
+            if (this.pagesToVisit.isEmpty()) {
+                currentUrl = url;
+                this.pagesVisited.add(url);
+            } else {
+                currentUrl = this.nextUrl();
+            }
+            leg.crawl(currentUrl);
+            boolean success = leg.searchForWord(searchWord);
+            if (success) {
+                System.out.println(String.format("**Success** Word %s found at %s", searchWord, currentUrl));
+                break;
+            }
+            this.pagesToVisit.addAll(leg.getLinks());
         }
+        System.out.println(String.format("**Done** Visited %s web page(s)", this.pagesVisited.size()));
     }
 
 
